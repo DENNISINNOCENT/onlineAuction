3677b973e52f54255e0af0b82b2b9a72313186ea
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-10-12 05:31:39 +0300

    more details added to our commodity

--- auction-backend/db/migrate/20221012023031_create_commodities.rb
+++ auction-backend/db/migrate/20221012023031_create_commodities.rb
@@ -2,10 +2,11 @@ class CreateCommodities < ActiveRecord::Migration[7.0]
   def change
     create_table :commodities do |t|
       t.string :name
-      t.integer :bid
+      t.string :category
+      t.integer :bidding_price
       t.string :image_url
       t.string :description
-      t.string :category
+      t.string :email_of_auctioner
 
       t.timestamps
     end
